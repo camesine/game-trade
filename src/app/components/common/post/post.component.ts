7dca94b9b28285b3b111b2366eef42af3604e24f
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-08-15 15:12:17 -0300

    Diseño post carousel

--- src/app/components/common/post/post.component.ts
+++ src/app/components/common/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
@@ -10,6 +12,37 @@ export class PostComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      jQuery('.slick_demo_2').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
   }
 
 }
